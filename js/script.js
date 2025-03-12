ce27ba35391f925f7b4fc886ef52e0578498d412
/echo <lars.pettersen91@gmail.com>
2025-03-12 17:01:51 +0100

    completed and crashed my computer making it count from 10 to 0 going up :I

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Question 1
+
+////Write a loop that logs the numbers 0 to 7.
+
+for (var i = 0; i <= 7; i++) {
+  console.log("Question 1:", i);
+}
+
+//Question 2
+
+//Write a loop that logs the numbers 10 to 1. Count down from 10 to 1.
+
+for (var count = 10; count >= 1; count--) {
+  console.log("Question 2:", count);
+}
+
+//Question 3
+
+//Write a loop that counts from 10 to 20, and log only the odd numbers. 11, 13, 15, etc
+
+for (var countAgain = 10; countAgain <= 20; countAgain++) {
+  console.log("Question 3:", countAgain);
+}
